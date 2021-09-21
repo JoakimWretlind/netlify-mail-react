f870fc3284593481b31d4cb1cc03087719cf9a91
Joakim Wretlind <joakim.wretlind@gmail.com>
2021-09-21 13:33:56 +0200

    added folder-structure for contact

--- src/App.js
+++ src/App.js
@@ -1,24 +1,10 @@
 import './App.css';
+import { Contact } from './components/index';
 
 function App() {
   return (
     <div className="App">
-      <h1>form</h1>
-      <form name="contact" method="post">
-        <input type="hidden" name="form-name" value="contact" />
-        <p>
-          <label>Your Name: <input type="text" name="name" /></label>
-        </p>
-        <p>
-          <label>Your Email: <input type="email" name="email" /></label>
-        </p>
-        <p>
-          <label>Message: <textarea name="message"></textarea></label>
-        </p>
-        <p>
-          <button type="submit">Send</button>
-        </p>
-      </form>
+      <Contact />
     </div>
   );
 }
